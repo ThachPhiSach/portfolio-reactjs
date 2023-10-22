f2b520a069ae74215d799821239412340db461d6
ThachPhiSach <phisachtv2017@gmail.com>
2023-10-23 03:15:16 +0700

    first commit

---
+++ reactfolio-master/reactfolio-master/src/data/user.js
@@ -0,0 +1,83 @@
+const INFO = {
+	main: {
+		title: "Reactfolio by Phi Sách",
+		
+	},
+
+	socials: {
+		youtube:"https://www.youtube.com/@phisach3524",
+		github: "https://github.com/ThachPhiSach",
+		instagram: "https://instagram.com/tpi.sach?igshid=OGQ5ZDc2ODk2ZA==",
+		facebook: "https://www.facebook.com/PhiSach.T.02",
+	},
+
+	about: {
+		title: "Thông tin bản thân và mục tiêu nghề nghiệp",
+		description:
+			"Chào mọi người, mình là Phi Sách, hiện đang là sinh viên năm cuối ngành cơ điện tử,mình đam mê lập trình và công nghệ từ nhỏ.Hiện tại mình đang học tại Trường Đại học SPKT TP.HCM. Qua những năm tháng được tiếp thu kiến thức và trải nghiệm thực tế, mình đã dần trưởng thành và có được sự tự tin để theo đuổi ước mơ trở thành một lập trình viên. Sau khi tốt nghiệp, mình mong muốn tìm được một công việc đúng với chuyên ngành mình dang theo đuổi, công việc mà mình có thể vận dụng những kiến thức đã học vào thực tiễn, đồng thời không ngừng học hỏi để nâng cao kỹ năng. Mình cũng muốn có cơ hội tham gia các dự án lớn, thử thách bản thân và khẳng định năng lực của mình. Mình tin với sự nỗ lực và đam mê, mình sẽ đạt được mục tiêu nghề nghiệp của mình trong tương lai. Cảm ơn bạn đã đọc giới thiệu về bản thân mình",
+	},
+
+	articles: {
+		title: "",
+		description:
+			"",
+	},
+
+	projects: [
+		{
+			title: "Project 1",
+			description:
+				"Bài tập tuần 3 ",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/java/java.png",
+			linkText: "View Project",
+			link: "https://www.facebook.com/PhiSach.T.02",
+		},
+
+		{
+			title: "Project 2",
+			description:
+				"Bài tập tuần 4",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/java/java.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 3",
+			description:
+				"Bài tập tuần 6",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/java/java.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 4",
+			description:
+				"Bài tập tuần 7",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/java/java.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 5",
+			description:
+				"Bài tập tuần 9",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/java/java.png",
+			linkText: "View Project",
+			link: "https://github.com",
+
+		},
+		{
+		title: "Project 6",
+			description:
+				"Bài tập tuần 9",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/java/java.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+	],
+};
+
+export default INFO;
